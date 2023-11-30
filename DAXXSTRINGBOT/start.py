94ab5309c4dff7fa515cb9f98aa6352ab77e2d87
adityajsru17 <148866502+adityajsru17@users.noreply.github.com>
2023-11-30 21:12:47 +0530

    Update start.py

--- DAXXSTRINGBOT/start.py
+++ DAXXSTRINGBOT/start.py
@@ -12,24 +12,26 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""𝐇𝐞𝐲 {msg.from_user.mention}🍷,
+        text=f"""𝐇𝐞𝐲 {msg.from_user.mention} 🍷,
 
-𝐈 𝐀𝐦 {me2},
-𝐓𝐑𝐔𝐒𝐓𝐄𝐃 𝐒𝐓𝐑𝐈𝐍𝐆 𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐎𝐑 𝐁𝐎𝐓.
-𝐅𝐔𝐋𝐋𝐘 𝐒𝐀𝐅𝐄 & 𝐒𝐄𝐂𝐔𝐑𝐄.
-𝐍𝐎  𝐄𝐑𝐑𝐎𝐑.
+𝙸 𝙰𝙼 {me2},
 
-𝐌𝐚𝐝𝐞 𝐁𝐲  : [𝐓𝐄𝐀𝐌 𝐃𝐀𝐗𝐗](tg://user?id={OWNER_ID}) !""",
+➳ 𝚃𝚁𝚄𝚂𝚃𝙴𝙳 𝚂𝚃𝚁𝙸𝙽𝙶 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙾𝚁 🫧
+
+➳ 𝙵𝚄𝙻𝙻𝚈 𝚂𝙰𝙵𝙴 𝙰𝙽𝙳 𝚂𝙴𝙲𝚄𝚁𝙴 ⚡️
+
+➳ 𝙽𝙾 𝙴𝚁𝚁𝙾𝚁 💥
+
+𝙼𝙰𝙳𝙴 𝙱𝚈 : 𝗔𝙽𝙾𝙼𝚈𝙽𝙾𝚄𝚂 𝗦𝙰𝚅𝙸𝙾𝚄𝚁𝚂
+
+""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="🌿𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆🌿", callback_data="generate")
-                ],
-                [
-                    InlineKeyboardButton(" 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url="https://t.me/AllPremiumCc"),
-                    InlineKeyboardButton("𝐎𝐟𝐟𝐢𝐜𝐞", url="https://t.me/CYBERDAXXX")
+                    InlineKeyboardButton(text="🌿 𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄 𝐒𝐓𝐑𝐈𝐍𝐆 🌿", callback_data="generate")
                 ]
             ]
         ),
+        
         disable_web_page_preview=True,
     )
